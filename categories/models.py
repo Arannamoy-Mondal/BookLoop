aa5c2659291067d03b2b766269da823e6aed1972
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-10-18 00:34:14 +0600

    add categories apps

---
+++ categories/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User as UserModel
+# Create your models here.
+
+class Category(models.Model):
+      title=models.CharField(max_length=150,null=True)
+      added_by=models.ForeignKey(UserModel,on_delete=models.CASCADE)
+      add_date=models.DateTimeField(auto_now=True)
+     
