1f720c748ce14a62862c90a093413b9c50928483
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-10-20 02:46:32 +0600

    add:delete_review

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import createReview,reviewHistory,allReviewHistory
+from .views import createReview,reviewHistory,allReviewHistory,deleteReview
 urlpatterns=[
     path("create/<int:id>",createReview,name="create_review"),
     path("review_history",reviewHistory,name="review_history"),
-    path("all_review_history",allReviewHistory,name="all_review_history")
+    path("all_review_history",allReviewHistory,name="all_review_history"),
+    path("delete_review/<int:id>",deleteReview,name="delete-review")
 ]
